a1915bf6948c8ad76ca9fdea338a6a91fd285690
Nickolai Yegorov <e-nicko@ya.ru>
2024-05-01 00:15:23 +0300

    chore: simple python website demo

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
